094fa64bb949ab4749355315a54d25de9857c635
sanjeevnetscore <smudela@netscoretech.com>
2026-06-01 16:13:57 +0530

    Delete utils directory

--- utils/date.js
+++
@@ -1,9 +0,0 @@
-function calculateRemainingDays(planEndDate) {
-  const end = new Date(planEndDate);
-  const today = new Date();
-
-  const diffTime = end - today;
-  return Math.max(0, Math.ceil(diffTime / (1000 * 60 * 60 * 24)));
-}
-
-module.exports = { calculateRemainingDays };
